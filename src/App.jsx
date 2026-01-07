956615cfbc8dec7040a54ad1052eb963da72df62
Madhav Dodiya <105213289+MadhavDodiya@users.noreply.github.com>
2026-01-07 12:52:37 +0530

    At office

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
-import './App.css';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
+import Home from './Pages/Home.jsx';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Home />
       <Footer />
     </div>
   )
