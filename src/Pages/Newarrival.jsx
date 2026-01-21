7186e5c35f7d931e81aac98f0fa7fea11a968ed6
Madhav Dodiya <105213289+MadhavDodiya@users.noreply.github.com>
2026-01-21 11:36:27 +0530

    At Office / New arrival

--- src/Pages/Newarrival.jsx
+++ src/Pages/Newarrival.jsx
@@ -32,7 +32,7 @@ const Products = [
     category: "Fashion",
     inStock: true,
     img1: "../src/assets/images/imgi_38_green.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_36_brown.jpg",
     colorImages: {
       brown: "../src/assets/images/imgi_17_orange-1.jpg",
       pink: "../src/assets/images/imgi_19_black-1.jpg",
@@ -49,7 +49,7 @@ const Products = [
     category: "Men",
     inStock: true,
     img1: "../src/assets/images/imgi_20_white-3.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_37_purple.jpg",
     colorImages: {
       // orange: "../src/assets/images/imgi_17_orange-1.jpg",
       // black: "../src/assets/images/imgi_19_black-1.jpg",
@@ -66,7 +66,7 @@ const Products = [
     category: "Women",
     inStock: false,
     img1: "../src/assets/images/imgi_22_white-2.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_38_green.jpg",
     colorImages: {
       white: "../src/assets/images/imgi_17_orange-1.jpg",
       violet: "../src/assets/images/imgi_19_black-1.jpg",
@@ -83,7 +83,7 @@ const Products = [
     category: "Men",
     inStock: true,
     img1: "../src/assets/images/imgi_25_brown-2.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_39_light-green-1.jpg",
     colorImages: {
       orange: "../src/assets/images/imgi_17_orange-1.jpg",
       black: "../src/assets/images/imgi_19_black-1.jpg",
@@ -100,7 +100,7 @@ const Products = [
     category: "Fashion",
     inStock: true,
     img1: "../src/assets/images/imgi_39_light-green-1.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_40_light-green-2.jpg",
     colorImages: {
       brown: "../src/assets/images/imgi_17_orange-1.jpg",
       pink: "../src/assets/images/imgi_19_black-1.jpg",
@@ -117,7 +117,7 @@ const Products = [
     category: "Men",
     inStock: true,
     img1: "../src/assets/images/imgi_51_white-7.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_41_black-3.jpg",
     colorImages: {
       // orange: "../src/assets/images/imgi_17_orange-1.jpg",
       // black: "../src/assets/images/imgi_19_black-1.jpg",
@@ -134,7 +134,7 @@ const Products = [
     category: "Fashion",
     inStock: true,
     img1: "../src/assets/images/imgi_52_white-8.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_42_blue.jpg",
     colorImages: {
       white: "../src/assets/images/imgi_17_orange-1.jpg",
       violet: "../src/assets/images/imgi_19_black-1.jpg",
@@ -151,7 +151,7 @@ const Products = [
     category: "Fashion",
     inStock: false,
     img1: "../src/assets/images/imgi_56_brown-4.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_42_blue.jpg",
     colorImages: {
       orange: "../src/assets/images/imgi_17_orange-1.jpg",
       black: "../src/assets/images/imgi_19_black-1.jpg",
@@ -168,7 +168,7 @@ const Products = [
     category: "Denim",
     inStock: true,
     img1: "../src/assets/images/imgi_58_black-9.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_43_dark-blue.jpg",
     colorImages: {
       brown: "../src/assets/images/imgi_17_orange-1.jpg",
       pink: "../src/assets/images/imgi_19_black-1.jpg",
@@ -185,7 +185,7 @@ const Products = [
     category: "Dress",
     inStock: true,
     img1: "../src/assets/images/imgi_61_grey-2.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_44_white-6.jpg",
     colorImages: {
       // orange: "../src/assets/images/imgi_17_orange-1.jpg",
       // black: "../src/assets/images/imgi_19_black-1.jpg",
@@ -202,7 +202,7 @@ const Products = [
     category: "Fashion",
     inStock: true,
     img1: "../src/assets/images/imgi_65_black-11.jpg",
-    img2: "",
+    img2: "../src/assets/images/imgi_45_light-grey.jpg",
     colorImages: {
       white: "../src/assets/images/imgi_17_orange-1.jpg",
       violet: "../src/assets/images/imgi_19_black-1.jpg",
